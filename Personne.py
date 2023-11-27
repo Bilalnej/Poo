c182c3b1861ab9966c4465a686102b355a9258ce
Bilal Nejjara <37298878+Bilalnej@users.noreply.github.com>
2023-11-27 12:25:50 +0000

    Add files via upload

--- Personne.py
+++ Personne.py
@@ -1,10 +1,13 @@
 class Personne:
     def __init__(self, nom, prenom, age):
-        self.__nom= nom
-        self.__prenom= prenom
-        self.__age= age
+        self.__nom = nom
+        self.__prenom = prenom
+        self.__age = age
+
     def se_presenter(self):
-        #attention f avant le message
-        return (f"mon nom est {self.__nom} mon prenom est {self.__prenom} mon age est {self.__age}")
-P1= Personne("ali", "ahmed", 22)
-print (P1.se_presenter())
+        return (f"Mon nom est {self.__nom} mon prénom est: {self.__prenom} mon âge est: {self.__age}")
+
+P1 = Personne("Ali", "hazoka", "23")
+
+print(P1.se_presenter())
+
